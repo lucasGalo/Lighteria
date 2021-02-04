392625b2e8a001c9795f5acd2def91eba2592005
lucas_galo <lucas_d>
2021-02-04 08:02:54 -0300

    Fazendo o layout da pagina

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Lighteria</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerTitulo}>
+        <Text style={styles.titulo}>Lighteria</Text>
+        <View style={styles.containerSacola}>
+        <Image
+          source={require('./assets/images/icone-sacola.png')}
+          style={styles.image} />
+          </View>
+      </View>
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F0F4'
+  },
+  containerTitulo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 24
+  },
+  titulo:{
+    fontFamily: 'OpenSans-ExtraBold',
+    fontSize: 28
+  },
+  containerSacola:{
+    backgroundColor: '#fff',
+    padding: 18,
+    borderRadius:30
+  },
+  imagem: {
+    height: 30,
+    width: 30
+  }
+})
 
 export default App;
