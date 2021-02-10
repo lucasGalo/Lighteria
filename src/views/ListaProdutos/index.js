407ae34fce9300cc7a95c5f029cfb074f3132a0e
lucas_galo <lucas_d>
2021-02-10 10:58:49 -0300

    Arrumando ListaProdutos

--- src/views/ListaProdutos/index.js
+++ src/views/ListaProdutos/index.js
@@ -5,21 +5,23 @@ import Cabecalho from './components/Cabecalho';
 import { Item } from './components/Produto';
 
 const ListaProdutos = () => {
-    return <View style={StyleSheet.container}>
-        <FlatList
-            numColumns={2}
-            data={DATA}
-            renderItem={({ item }) => <Item {...item} />}
-            keyExtractor={item => item.id}
-            ListHeaderComponent={<Cabecalho />}
-        />
-    </View>
-}
+    return (
+        <View style={styles.container}>
+            <FlatList
+                numColumns={2}
+                data={DATA}
+                renderItem={({ item }) => <Item {...item} />}
+                keyExtractor={(item) => item.id}
+                ListHeaderComponent={<Cabecalho />}
+            />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         marginHorizontal: 24
     }
-})
+});
 
 export default ListaProdutos;
